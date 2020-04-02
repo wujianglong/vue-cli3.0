79244bede42f10434af975b922ebac457a7e4612
wujianglong <wujianglong@tonnn.com>
2020-04-02 21:39:17 +0800

    * 测试

--- src/router.js
+++ src/router.js
@@ -6,35 +6,34 @@ Vue.use(Router);
 export default new Router({
   mode: "history",
   base: "/",
-  routes: [
-    {
-      path: "/",
-      component: () => import("@/views/Layout.vue"),
-      children: [
-        {
-          path: "/test",
-          name: "Test",
-          component: () => import("@/views/Test.vue")
-        },
-        {
-          path: "/",
-          name: "login",
-          component: () => import("@/views/Login.vue")
-        },
-        {
-          path: "/about",
-          name: "about",
-          meta: {
-            requireAuth: true
-          },
-          component: () => import("@/views/About.vue")
-        },
-        {
-          path: "/result",
-          name: "result",
-          component: () => import("@/views/Result.vue")
-        }
-      ]
-    }
-  ]
+  routes: [{
+    path: "/",
+    component: () =>
+      import ("@/views/Layout.vue"),
+    children: [{
+        path: "/test",
+        name: "Test",
+        component: () =>
+          import ("@/views/Test.vue")
+      },
+      {
+        path: "/",
+        name: "login",
+        component: () =>
+          import ("@/views/Login.vue")
+      },
+      {
+        path: "/about",
+        name: "about",
+        component: () =>
+          import ("@/views/About.vue")
+      },
+      {
+        path: "/result",
+        name: "result",
+        component: () =>
+          import ("@/views/Test.vue")
+      }
+    ]
+  }]
 });
